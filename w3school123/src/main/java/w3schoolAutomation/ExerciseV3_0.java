21da5b7b3ffbb31c9e815fefee1b9c64cbcfbdfd
9405487695 <126315016+9405487695@users.noreply.github.com>
2023-02-25 19:30:20 +0530

    Update ExerciseV3_0.java
    change of pom class

--- w3school123/src/main/java/w3schoolAutomation/ExerciseV3_0.java
+++ w3school123/src/main/java/w3schoolAutomation/ExerciseV3_0.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ExerciseV3_0 {
+	//private WebDriver driveG;
 	private WebDriver driveG;
 	
 	@FindBy (xpath = "(//button[text()='Show Answer'])[2]")
