ff101ebd7f5968dcfc9f0ba5aef670998a980cbb
9405487695 <126315016+9405487695@users.noreply.github.com>
2023-02-25 19:35:24 +0530

    Update ExerciseV3_0.java
    as it is

--- w3school123/src/main/java/w3schoolAutomation/ExerciseV3_0.java
+++ w3school123/src/main/java/w3schoolAutomation/ExerciseV3_0.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ExerciseV3_0 {
-	//private WebDriver driveG;
 	private WebDriver driveG;
 	
 	@FindBy (xpath = "(//button[text()='Show Answer'])[2]")
